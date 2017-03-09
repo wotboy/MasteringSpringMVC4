04e7d385ac76a0cc48325793000856f855a6541a
Mike Prince <wotboy@gmail.com>
2017-03-09 10:17:43 -0800

    2nd commit

--- wednesday/src/main/java/wednesday/profile/ProfileController.java
+++ wednesday/src/main/java/wednesday/profile/ProfileController.java
@@ -1,5 +1,19 @@
 package wednesday.profile;
 
-public class ProfileController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+public class ProfileController {
+	@RequestMapping("/profile")
+	public String displayProfile(ProfileForm profileForm) {
+		return "profile/profilePage";
+	}
+	
+	@RequestMapping(value="/profile", method=RequestMethod.POST)
+	public String saveProfile(ProfileForm profileForm) {
+		System.out.println("save ok-- " + profileForm);
+		return "redirect:/profile";
+	}
 }
